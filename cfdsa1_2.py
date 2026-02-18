c59dc2793d27ddb167ee5c9c757c2c204855465d
Renuprakash K Palaniappan <renuprakash6@gmail.com>
2026-02-18 20:28:37 +0530

    prob1_2)

---
+++ cfdsa1_2.py
@@ -0,0 +1,5 @@
+value = int(input())
+for i in range (value,1001,value):    
+    print(i)
+          
+
---
+++ cfdsa1_2a.py
@@ -0,0 +1,5 @@
+value = int(input())
+mul = value
+while mul<= 1000:
+    print(mul)
+    mul += value
