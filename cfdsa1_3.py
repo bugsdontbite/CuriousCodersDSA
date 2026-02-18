bede443737be7371a6fa6675e33cdd00a1af82bc
Renuprakash K Palaniappan <renuprakash6@gmail.com>
2026-02-18 22:04:52 +0530

    prob3

---
+++ cfdsa1_3.py
@@ -0,0 +1,5 @@
+firstname=str(input("Enter first name: "))
+lastname=str(input("Last name: "))
+times=int(input("Times to repeat: "))
+print(times*(firstname+lastname+"\n"))
+          
---
+++ cfdsa1_3a.py
@@ -0,0 +1,6 @@
+firstname = input("Enter First Name: ")
+lastname = input("Enter Last Name : ")
+n=int(input("Enter the Number of times to repeat: "))
+for i in range (n):
+      print(firstname+lastname)
+      
