ceae82b56e8e20dba1575b448b9699da01fdfc75
Renuprakash K Palaniappan <renuprakash6@gmail.com>
2026-02-21 21:42:43 +0530

    Center diamond

---
+++ pattern_12.py
@@ -0,0 +1,16 @@
+n= int(input())
+for i in range (1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end="")
+    for j in range (1,(2*i)):
+        print("*",end="")
+    print()
+for i in range (1,n):
+    for j in range (1,i+1):
+        print(" ",end="")
+    for j in range (1,(2*(n-i))):
+        print("*",end="")
+    print()
+    
+        
+ 
